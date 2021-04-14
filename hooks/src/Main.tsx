face8c1fe26f227c2296e5467c12f6a0073adc41
yongwookLee <yongw00k@g.skku.edu>
2021-04-14 22:41:54 +0900

    Add Counter page using useReducer

--- hooks/src/Main.tsx
+++ hooks/src/Main.tsx
@@ -9,7 +9,10 @@ function Main({ match, history, location }: Props): JSX.Element {
   return (
     <div>
       <Link to="/useState/counter">useState : Counter</Link>
+      <br />
       <Link to="/useState/myform">useState : MyForm</Link>
+      <br />
+      <Link to="/useReducer/counter">useReducer : Counter</Link>
     </div>
   );
 }
--- hooks/src/Router.tsx
+++ hooks/src/Router.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Main from './Main';
 import Counter from './useState/Counter';
 import MyForm from './useState/MyForm';
+import CounterReducer from './useReducer/Counter';
+
 function Router(): JSX.Element {
   return (
     <BrowserRouter>
@@ -10,6 +12,7 @@ function Router(): JSX.Element {
         <Route exact={true} path="/" component={Main}></Route>
         <Route path="/useState/counter" component={Counter}></Route>
         <Route path="/useState/myform" component={MyForm}></Route>
+        <Route path="/useReducer/counter" component={CounterReducer}></Route>
         {/* Not Found */}
         <Route component={() => <Redirect to="/" />} />
       </Switch>
